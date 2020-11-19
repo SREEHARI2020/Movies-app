d35fd7e9c194e3093a83af9a6f5bd5a7f99a1d2d
SREEHARI <sreehari2610@gmail.com>
2020-11-19 20:53:46 +0530

    React-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
+import React from 'react'
+import{BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {Header} from './components/Header';
+import {Watchlist} from './components/Watchlist';
+import {Watched} from './components/Watched';
+import {Add} from './components/Add';
+import 'font-awesome/css/font-awesome.css';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+<Header />
+<Switch>
+<Route exact path='/'><Watchlist /></Route>
+<Route path='/watched'><Watched /></Route>
+<Route  path='/add'><Add /></Route>
+</Switch>
+
+    </Router>
+    
+    
   );
 }
 
